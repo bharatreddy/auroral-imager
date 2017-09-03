62ce1fcf0c1fda917c60f93d9644e97fb7e6510d
bharatreddy <bharatr@vt.edu>
2017-09-03 17:23:49 -0400

    to df

--- py/read_ssusi.py
+++ py/read_ssusi.py
@@ -1,4 +1,5 @@
 import netCDF4
+from cdf.internal import EPOCHbreakdown
 import pandas
 import os
 import numpy
@@ -34,16 +35,30 @@ class ReadData(object):
         """
         for currFile in self.fileList:
             currDataSet = netCDF4.Dataset(currFile)
-            print currFile
-            print currDataSet.variables["TIME_DAY"].shape
-
-            epochList = currDataSet.variables["TIME_EPOCH_DAY"][:]
-            # account for difference in seconds between
-            # CDF epoch and python's epoch, leap year in there
-            # (datetime(1971,1,2) - 
-            #      datetime(1,1,1)).total_seconds()*1000
-            epochList = epochList - 62167219200000
-            a = epochList[0]
-            print datetime.datetime.fromtimestamp(a/1e3)
+            # get datetime from epoch list
+            dtList = numpy.array( [ datetime.datetime( *EPOCHbreakdown( e ) ) \
+                        for e in currDataSet.variables["TIME_EPOCH_DAY"][:] ] )
+            # get peircepoints
+            prpntLats = currDataSet.variables['PIERCEPOINT_DAY_LATITUDE'][:]
+            prpntLons = currDataSet.variables['PIERCEPOINT_DAY_LONGITUDE'][:]
+            prpntAlts = currDataSet.variables['PIERCEPOINT_DAY_ALTITUDE'][:]
+            # GET DISK intensity data - waveband/color radiance data
+            # 5 colors are - 121.6, 130.4, 135.6 nm and LBH short and LBH long
+            dskInt121 = currDataSet.variables['DISK_INTENSITY_DAY'][:, :, 0]
+            dskInt130 = currDataSet.variables['DISK_INTENSITY_DAY'][:, :, 1]
+            dskInt135 = currDataSet.variables['DISK_INTENSITY_DAY'][:, :, 2]
+            dskIntLBHS = currDataSet.variables['DISK_INTENSITY_DAY'][:, :, 3]
+            dskIntLBHL = currDataSet.variables['DISK_INTENSITY_DAY'][:, :, 4]
+            # We'll store the data in a DF. We need to convert the dates into a 
+            # similar shape as disk radiance.
+            dateArr = numpy.asarray( [dtList]*dskInt121.shape[0] )
+            # store data in a DF
+            ssusiDF = pandas.DataFrame({ 'glat': prpntLats.ravel(),\
+                         'glon': prpntLons.ravel(),'di121': dskInt121.ravel(),\
+                          'di130': dskInt130.ravel(),'di135': dskInt135.ravel(),\
+                          'diLBHS': dskIntLBHS.ravel(),'diLBHL': dskIntLBHL.ravel(),\
+                          'date':dateArr.ravel()})
             break
+        return ssusiDF
+
 
